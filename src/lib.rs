d89b23e595bd31981cc72d8af2c713a46ae997f7
Kurt Lawrence <kurtlawrence92@gmail.com>
2021-08-30 16:29:19 +1000

    bring in progress and switch (#1)

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
 //! Core common items that are required by _all_ other crates in the `daedalus` crate graph.
 #![warn(missing_docs)]
 
+mod progress;
 mod str;
+mod switch;
 
 #[doc(inline)]
 pub use crate::str::Str;
+#[doc(inline)]
+pub use crate::switch::Switch;
+#[doc(inline)]
+pub use progress::*;
